36b3b5c12c8f34910b36b59c7d80548cc3bc2925
Shanaka <shanaka@Shanaka-2.local>
2019-01-28 19:51:50 +0530

    Update user-login.component.ts
    modifications for UI changes

--- src/app/user/user-login/user-login.component.ts
+++ src/app/user/user-login/user-login.component.ts
@@ -3,34 +3,36 @@ import { WsCallback } from 'src/app/util/ws-callback';
 import { WsType } from 'src/app/util/ws-type';
 import { WsResponse } from 'src/app/util/ws-response.model';
 import { UserServiceService } from '../user-service.service';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-user-login',
-  templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.css']
+	selector: 'app-user-login',
+	templateUrl: './user-login.component.html',
+	styleUrls: ['./user-login.component.css']
 })
 export class UserLoginComponent implements OnInit, WsCallback {
-  public userName: string;
-  public password: string;
+	public userName: string;
+	public password: string;
 
-  constructor(private userService: UserServiceService) { }
+	constructor(private userService: UserServiceService, private router: Router) { }
 
-  ngOnInit() {
-  }
+	public clickOnSignInButton() {
+		console.log('click on the save button, uname=' + this.userName + ' pass=' + this.password);
+		// const request = { userName: this.userName, password: this.password };
+		// console.log(request);
+		// this.userService.login(request, this);
+		this.router.navigateByUrl('home');
+	}
 
-  public clickOnSignInButton() {
-    console.log('click on the save button, uname=' + this.userName + ' pass=' + this.password);
-    const request = {userName: this.userName, password: this.password};
-    console.log(request);
-    this.userService.login(request, this);
-  }
+	onSuccess(data: WsResponse, serviceType: WsType) {
+		console.log('success ' + data);
+		// this.router.navigateByUrl('home');
+	}
 
-  onSuccess(data: WsResponse, serviceType: WsType) {
-    console.log('success ' + data);
-  }
+	onFail(data: WsResponse, serviceType: WsType) {
+		console.log('error');
+	}
 
-  onFail(data: WsResponse, serviceType: WsType) {
-    console.log('error');
-  }
-
+	ngOnInit() {
+	}
 }
