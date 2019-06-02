f66f2a5d60af9495b81da49e76f5bb81d3135f89
Indika Gunawardana <indika.gunawardana@gmail.com>
2019-06-02 14:45:04 +0530

    new types

--- src/app/services/util/ws-type.ts
+++ src/app/services/util/ws-type.ts
@@ -4,4 +4,9 @@ export class WsType {
     public static GET_SERVICES_BY_USER_ID = 3;
     public static GET_GROUPS_BY_USER_ID = 4;
     public static GET_SERVICES_BY_GROUP_ID = 5;
+    public static ADD_SERVICE = 6;
+    public static GET_SERVICE_BY_SERVICE_ID = 7;
+    public static DELETE_SERVICE = 8;
+    public static PUBLISH_SERVICE = 9;
+    public static UNPUBLISH_SERVICE = 10;
 }
